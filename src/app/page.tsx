fbd91d1b27b3f71f2a95ab8750b7e5063cefed03
kangtlee <16286841+punkzberryz@users.noreply.github.com>
2024-07-21 18:15:38 +0700

    add redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image";
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect("/admin");
+  redirect("/auth/signin");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Home</h1>
