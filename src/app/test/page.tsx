98b82f2eeecb61e74aa5db2ae86b2466e9847679
kangtlee <16286841+punkzberryz@users.noreply.github.com>
2024-07-24 10:20:32 +0700

    add test page

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,9 +1,13 @@
 import { UnauthorizedMessageCode } from "@/components/error-ui";
+import { validateRequest } from "@/lib/auth";
 import { UnauthorizedError } from "@/lib/error";
 import React from "react";
 
-const TestPage = () => {
-  throw new UnauthorizedError(UnauthorizedMessageCode.notAdmin);
+const TestPage = async () => {
+  const { user } = await validateRequest();
+  if (user?.role !== "ADMIN") {
+    throw new UnauthorizedError(UnauthorizedMessageCode.notAdmin);
+  }
   return <div>TestPage</div>;
 };
 
